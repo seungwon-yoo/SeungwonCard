b30a306d51797d7b5f940d140918a1754b278f0e
seungwon-yoo <qwer15417@gmail.com>
2022-02-01 15:16:25 +0900

    Initial Commit

---
+++ SeungwonCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeungwonCard
+//
+//  Created by 유승원 on 2022/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeungwonCard/SeungwonCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeungwonCardApp.swift
+//  SeungwonCard
+//
+//  Created by 유승원 on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct SeungwonCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
